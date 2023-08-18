f32546be77b3ac259a114e43f17ba6d61f3ae916
Muhammad Yasir <ybabar1@gmail.com>
2023-08-18 22:38:21 +0500

    comments

--- crud_app/src/components/displayUsers/DisplayUsers.js
+++ crud_app/src/components/displayUsers/DisplayUsers.js
@@ -1,3 +1,8 @@
+/**
+ * Best practice is generally to divide data mapping in two components
+ *  1. Logical component, where you fetch your data like this one
+ *  2. UI Component where you just display your data like UserCard
+ */
 import React from "react";
 import { useSelector } from "react-redux";
 import UserCard from "./UserCard";
@@ -7,9 +12,11 @@ const DisplayUsers = () => {
 
   return (
     <div className="displayUsers">
-      {usersList.map((user) => (
-        <UserCard user={user} key={user.id} />
-      ))}
+      {usersList?.length < 1 ? (
+        <h1>NO RECORD FOUND</h1>
+      ) : (
+        usersList.map((user) => <UserCard user={user} key={user.id} />)
+      )}
     </div>
   );
 };
